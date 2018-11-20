fd5bfac9b0c4cf981a1f4cbc9e100d930d515da9
Nicolas <nicolaskrusser@gmail.com>
2018-11-20 12:31:02 +0100

    created main component with charts.js

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import classes from './Header.css';
-import NavigationItem from '../Navigation/NavigationItem/NavigationItem';
 import Logo from '../UI/Logo/Logo';
 import Avatar from '../UI/Avatar/Avatar';
 import MessageBtn from '../UI/MessageBtn/MessageBtn';
@@ -8,9 +7,10 @@ import MessageBtn from '../UI/MessageBtn/MessageBtn';
 const header = (props)=>(
     <header className={classes.Header}>
         <Logo/>
-        <NavigationItem link='/' exact>Main</NavigationItem>
-        <MessageBtn/>
-        <Avatar/>
+        <div>
+            <MessageBtn/>
+            <Avatar/>
+        </div>
     </header>
 );
 
--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -1,14 +1,87 @@
 import React, {Component} from 'react';
+import {Line, Bar} from 'react-chartjs-2';
 
 class Main extends Component{
     state = {
-    
+        prodVolData:{
+            labels: [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            ],
+            datasets:[
+                {
+                    label: 'Employers amount',
+                    data:[142, 164, 146, 184, 154, 187, 195, 148, 164, 164, 187, 178],
+                    backgroundColor: [
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)",
+                        "rgb(65, 131, 196)"
+                    ]
+                },
+                {
+                    label: 'Production Volumes',
+                    data:[142, 164, 146, 184, 154, 187, 195, 148, 164, 164, 187, 178],
+                    backgroundColor: [
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)",
+                        "rgb(79, 83, 99)"
+                    ]
+                }
+            ]
+        },
+        productivityRatio: 1.6
+    }
+
+    componentDidMount(){
+        this.productivityRatio();
+    }
+
+    productivityRatio = ()=>{
+        
     }
 
     render(){
+        console.log(this.state.prodVolData.datasets[0].data)
         return(
             <section>
-                main
+                <Bar
+                    data={this.state.prodVolData}
+                    width={100}
+                    height={400}
+                    options={{
+                        maintainAspectRatio: false
+                    }}
+                />
             </section>
         );
     }
