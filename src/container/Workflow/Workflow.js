eb8c080afd38e3dac429575f55769aa348123adf
Nicolas <nicolaskrusser@gmail.com>
2018-11-24 11:14:11 +0100

    added chart Line and styles

--- src/container/Workflow/Workflow.js
+++ src/container/Workflow/Workflow.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Line} from 'react-chartjs-2';
 import * as action from '../../store/actions/actionTypes';
 
 import Input from '../../components/UI/Input/Input';
@@ -7,21 +8,48 @@ import Button from '../../components/UI/Button/Button';
 
 import classes from './Workflow.css';
 
-
-
 class Workflow extends Component{
     
     render(){
-    
+        console.log(this.props.prodVolData)
         const months = this.props.prodVolData.labels;
         let employers = this.props.prodVolData.datasets[0].data;
 
         let employersList = employers.map(employer=>{
             return <li key={employer * Math.random()}>{employer}</li>
         })
+
         let monthsList = months.map(month=>{
             return <li key={month}>{month}</li>
         })
+
+        const data = {
+            labels: this.props.prodVolData.labels,
+            datasets: [
+                {
+                  label: this.props.prodVolData.datasets[0].label,
+                  fill: false,
+                  lineTension: 0.1,
+                  backgroundColor: '#1e70bf',
+                  borderColor: '#1e70bf',
+                  borderCapStyle: 'butt',
+                  borderDash: [],
+                  borderDashOffset: 0.0,
+                  borderJoinStyle: 'miter',
+                  pointBorderColor: '#1e70bf',
+                  pointBackgroundColor: '#1e70bf',
+                  pointBorderWidth: 1,
+                  pointHoverRadius: 5,
+                  pointHoverBackgroundColor: '#1e70bf',
+                  pointHoverBorderColor: '#1e70bf',
+                  pointHoverBorderWidth: 2,
+                  pointRadius: 1,
+                  pointHitRadius: 10,
+                  data: this.props.prodVolData.datasets[0].data
+                }
+            ]
+        }
+
         return(
             <section className={classes.Workflow}>
                 <div className={classes.Table}>
@@ -31,6 +59,17 @@ class Workflow extends Component{
                     <ul>
                         {employersList}
                     </ul>
+                    <div className={classes.Line}>
+                        <Line 
+                            data={data}
+                            width={100}
+                            height={100}
+                            options={{
+                                maintainAspectRatio: false,
+                                title: this.props.prodVolData.options.title
+                            }}
+                        />
+                    </div>
                 </div>
             </section>
         );
